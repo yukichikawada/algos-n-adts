92c86d6f272631a7952ba7ac155429f5dfb3e3ed
Yukichi Kawada <yukichikawada@gmail.com>
2018-05-05 23:59:32 -0700

    BST insert not going as expected

--- binary_search_trees/lib/binary_search_tree.rb
+++ binary_search_trees/lib/binary_search_tree.rb
@@ -1,11 +1,19 @@
-# There are many ways to implement these methods, feel free to add arguments 
+# There are many ways to implement these methods, feel free to add arguments
 # to methods as you see fit, or to create helper methods.
+require 'bst_node'
 
 class BinarySearchTree
+  attr_reader :root
+
   def initialize
+    @root = nil
   end
 
   def insert(value)
+    if @root == nil
+      @root = BSTNode.new(value)
+    end
+    insert_recursive(@root, value)
   end
 
   def find(value, tree_node = @root)
@@ -19,7 +27,7 @@ class BinarySearchTree
   end
 
   def depth(tree_node = @root)
-  end 
+  end
 
   def is_balanced?(tree_node = @root)
   end
@@ -31,4 +39,14 @@ class BinarySearchTree
   private
   # optional helper methods go here:
 
+  def insert_recursive(node, value)
+    if node.nil?
+      node = BSTNode.new(value)
+    elsif node.value > value
+      node.left = insert_recursive(node.left, value)
+    else
+      node.right = insert_recursive(node.right, value)
+    end
+    node
+  end
 end
