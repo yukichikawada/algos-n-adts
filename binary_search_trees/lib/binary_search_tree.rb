0e34e772d0030361923d5556659b4b55acd615d0
Yukichi Kawada <yukichikawada@gmail.com>
2018-05-06 23:10:24 -0700

    BST#delete passing specs

--- binary_search_trees/lib/binary_search_tree.rb
+++ binary_search_trees/lib/binary_search_tree.rb
@@ -12,18 +12,53 @@ class BinarySearchTree
   def insert(value)
     if @root == nil
       @root = BSTNode.new(value)
+    else
+      insert_recursive(@root, value)
     end
-    insert_recursive(@root, value)
   end
 
   def find(value, tree_node = @root)
+    return nil if tree_node.nil?
+    case value <=> tree_node.value
+    when -1
+      find(value, tree_node.left)
+    when 0
+      tree_node
+    else
+      find(value, tree_node.right)
+    end
   end
 
   def delete(value)
+    return @root = nil if @root.value == value
+    node = find(value)
+    parent = find_parent(value, @root)
+    if node.left.nil? && node.right.nil?
+      patch_parent(parent, nil, value)
+    elsif node.left && node.right.nil?
+      patch_parent(parent, node.left, value)
+    elsif node.left.nil? && node.right
+      patch_parent(parent, node.right, value)
+    elsif node.left && node.right
+      max_node = maximum(node.left)
+      max_node_parent = find_parent(max_node.value)
+      patch_parent(parent, max_node, value)
+      if max_node.left
+        max_child = max_node.left
+        patch_parent(max_node_parent, max_child, max_node.value)
+      else
+        max_child = max_node.right
+        patch_parent(max_node_parent, max_child, max_node.value)
+      end
+    end
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    until tree_node.right.nil?
+      tree_node = tree_node.right
+    end
+    tree_node
   end
 
   def depth(tree_node = @root)
@@ -49,4 +84,24 @@ class BinarySearchTree
     end
     node
   end
+
+  def find_parent(value, node = @root)
+    return nil if node.nil?
+    if node.left.value == node.value || node.right.value == node.value
+      node
+    elsif node.value > value
+      find_parent(value, node.left)
+    else
+      find_parent(value, node.right)
+    end
+  end
+
+  def patch_parent(parent, new_child, value)
+    if parent.left.value == value
+      parent.left = new_child
+    elsif parent.right.value == value
+      parent.right = new_child
+    end
+  end
+
 end
