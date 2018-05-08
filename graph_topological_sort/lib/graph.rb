2b4e380dbe8f228cb9e215464d74aa9b9347170c
Yukichi Kawada <yukichikawada@gmail.com>
2018-05-08 00:00:28 -0700

    Add vertex and edge classes

--- graph_topological_sort/lib/graph.rb
+++ graph_topological_sort/lib/graph.rb
@@ -1,15 +1,28 @@
 class Vertex
-  def initialize(value)
+  attr_accessor :value, :in_edges, :out_edges
 
+  def initialize(value)
+    @value = value
+    @in_edges = []
+    @out_edges = []
   end
 end
 
 class Edge
-  def initialize(from_vertex, to_vertex, cost = 1)
+  attr_accessor :from_vertex, :to_vertex, :cost
 
+  def initialize(from_vertex, to_vertex, cost = 1)
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @cost = cost
+    @to_vertex.in_edges.push(self)
+    @from_vertex.out_edges.push(self)
   end
 
   def destroy!
-
+    @to_vertex.in_edges = []
+    @from_vertex.out_edges = []
+    @from_vertex = nil
+    @to_vertex = nil
   end
 end
--- graph_topological_sort/lib/topological_sort.rb
+++ graph_topological_sort/lib/topological_sort.rb
@@ -3,5 +3,15 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  sorted = []
+  queue = []
 
+  vertices.each do |vertex|
+    if vertex.in_edges.empty?
+      queue.push(vertex)
+    end
+  end
+  
+
+  sorted
 end
