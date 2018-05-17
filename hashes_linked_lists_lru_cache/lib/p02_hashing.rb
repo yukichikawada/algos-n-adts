0cb0f6dfb9aff6631c31147a03a0af22ee098f2c
Yukichi Kawada <yukichikawada@gmail.com>
2018-05-16 23:45:17 -0700

    Passing hashing specs

--- hashes_linked_lists_lru_cache/lib/p02_hashing.rb
+++ hashes_linked_lists_lru_cache/lib/p02_hashing.rb
@@ -4,11 +4,19 @@ end
 
 class Array
   def hash
+    sum = 0
+
+    self.each_with_index do |el, idx|
+      sum += el.hash ^ idx.hash
+    end
+
+    sum
   end
 end
 
 class String
   def hash
+    self.split("").map(&:ord).hash
   end
 end
 
@@ -16,6 +24,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort.hash
   end
 end
